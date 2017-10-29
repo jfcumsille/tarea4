c262c1447084845b93489c1abac542e8cb7e23c4
jfcumsille <jfcumsille@uc.cl>
2017-10-28 23:35:15 -0300

    imprime tablero con fichas

--- src/chess/main.c
+++ src/chess/main.c
@@ -1,3 +1,12 @@
+#include "../lib/functions.h"
 
+
+int main(int argc, char argv[]){
+
+    Board *b = initBoard();
+    print_table(b);
+
+    return 0;
+};
 
 
--- src/lib/functions.c
+++ src/lib/functions.c
@@ -3,6 +3,9 @@
 //
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <locale.h>
 #include "functions.h"
 
 Piece* initPiece(int id, int color, int row, int col){
@@ -17,7 +20,6 @@ Piece* initPiece(int id, int color, int row, int col){
 
 Board* initBoard(){
     Board *b = malloc(sizeof(Board));
-    Piece* table[8][8];
     Piece* bT1 = initPiece(3, 1, 0, 0);
     Piece* bH1 = initPiece(5, 1, 0, 1);
     Piece* bA1 = initPiece(4, 1, 0, 2);
@@ -26,14 +28,14 @@ Board* initBoard(){
     Piece* bA2 = initPiece(4, 1, 0, 5);
     Piece* bH2 = initPiece(5, 1, 0, 6);
     Piece* bT2 = initPiece(3, 1, 0, 7);
-    table[0][0] = bT1;
-    table[0][1] = bH1;
-    table[0][2] = bA1;
-    table[0][3] = bQ;
-    table[0][4] = bK;
-    table[0][5] = bA2;
-    table[0][6] = bH2;
-    table[0][7] = bT2;
+    b->table[0][0] = bT1;
+    b->table[0][1] = bH1;
+    b->table[0][2] = bA1;
+    b->table[0][3] = bQ;
+    b->table[0][4] = bK;
+    b->table[0][5] = bA2;
+    b->table[0][6] = bH2;
+    b->table[0][7] = bT2;
 
     Piece* wT1 = initPiece(3, 0, 7, 0);
     Piece* wH1 = initPiece(5, 0, 7, 1);
@@ -43,49 +45,64 @@ Board* initBoard(){
     Piece* wA2 = initPiece(4, 0, 7, 5);
     Piece* wH2 = initPiece(5, 0, 7, 6);
     Piece* wT2 = initPiece(3, 0, 7, 7);
-    table[7][0] = wT1;
-    table[7][1] = wH1;
-    table[7][2] = wA1;
-    table[7][3] = wQ;
-    table[7][4] = wK;
-    table[7][5] = wA2;
-    table[7][6] = wH2;
-    table[7][7] = wT2;
+    b->table[7][0] = wT1;
+    b->table[7][1] = wH1;
+    b->table[7][2] = wA1;
+    b->table[7][3] = wQ;
+    b->table[7][4] = wK;
+    b->table[7][5] = wA2;
+    b->table[7][6] = wH2;
+    b->table[7][7] = wT2;
 
     for(int i=0;i<8;i++){
         Piece* bP = initPiece(6, 1, 1, i);
         Piece* wP = initPiece(6, 0, 6, i);
-        table[1][i] = bP;
-        table[6][i] = wP;
+        b->table[1][i] = bP;
+        b->table[6][i] = wP;
     }
     for(int i=2;i<6;i++){
         for(int j=0;j<8;j++){
-            table[i][j] = 0;
+            b->table[i][j] = 0;
         }
     }
+    return b;
 }
 
 wchar_t toASCII(Piece* piece){
     wchar_t ascii;
     if (piece->color == 0){
-        if(piece->id == 1) ascii = '256A';
-        else if(piece->id == 2) ascii = '256B';
-        else if(piece->id == 3) ascii = '256C';
-        else if(piece->id == 4) ascii = '256D';
-        else if(piece->id == 5) ascii = '256E';
-        else ascii = '256F';
+        if(piece->id == 1) ascii = L'\x256A';
+        else if(piece->id == 2) ascii = L'\x256B';
+        else if(piece->id == 3) ascii = L'\x256C';
+        else if(piece->id == 4) ascii = L'\x256D';
+        else if(piece->id == 5) ascii = L'\x256E';
+        else ascii = L'\x256F';
     }
     else{
-        if(piece->id == 1) ascii = '2564';
-        else if(piece->id == 2) ascii = '2565';
-        else if(piece->id == 3) ascii = '2566';
-        else if(piece->id == 4) ascii = '2567';
-        else if(piece->id == 5) ascii = '2568';
-        else ascii = '2569';
+        if(piece->id == 1) ascii = L'\x2564';
+        else if(piece->id == 2) ascii = L'\x2565';
+        else if(piece->id == 3) ascii = L'\x2566';
+        else if(piece->id == 4) ascii = L'\x2567';
+        else if(piece->id == 5) ascii = L'\x2568';
+        else ascii = L'\x2569';
     }
     return ascii;
 }
 
 void print_table(Board* tablero){
-
+    setlocale(LC_ALL, "en_US.UTF-8");
+    printf("\n\t  ________________________________\n");
+    for(int i=0;i<8;i++){
+        printf("\n");
+        printf("\t |");
+        for(int j=0;j<8;j++){
+            if(!tablero->table[i][j]) printf("   |");
+            else {
+                wchar_t cell = toASCII(tablero->table[i][j]);
+                printf(" %lc |", cell);
+            }
+        }
+        printf("\n");
+    }
+    printf("\t ---------------------------------\n");
 }
